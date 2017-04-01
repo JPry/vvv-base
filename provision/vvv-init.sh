bbed45746049f75eb55f94edce35dc79ed44e6ed
Jeremy Pry <jeremy.pry@gmail.com>
2017-04-01 13:41:55 -0400

    Update provision script to install WP

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
 # Change to the parent directory to run scripts.
-cd ..
-
-# List all variables
-set -o posix; set
+cd "${VM_DIR}"
 
 # Run composer
 noroot composer update
+
+# Maybe install WordPress
+if ! $(wp core is-installed); then
+	WP_ADMIN_USER=`get_config_value admin_user 'admin_default'`
+	WP_ADMIN_PASS=`get_config_value admin_password 'password_default'`
+	WP_ADMIN_EMAIL=`get_config_value admin_email 'admin@localhost.dev'`
+	WP_SITE_TITLE=`get_config_value title 'My Awesome Site'`
+	WP_HOST=`get_primary_host`
+
+	noroot wp core config --dbname="${SITE_ESCAPED}" --dbuser=wp --dbpass=wp --dbhost="localhost" --dbprefix=wp_ --locale=en_US --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG_LOG', true );
+define( 'SCRIPT_DEBUG', true );
+define( 'JETPACK_DEV_DEBUG', true );
+PHP
+
+	noroot wp core install --url="${WP_HOST}" --title="${WP_SITE_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASS}" --admin_email="${WP_ADMIN_EMAIL}"
+fi
+
+# Return to the previous directory
+cd -
