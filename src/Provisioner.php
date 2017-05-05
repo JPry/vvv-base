d9c5392c825f12474508c1768b66642e1be399a8
Jeremy Pry <jeremy.pry@gmail.com>
2017-05-05 18:05:59 -0400

    Start adding functionality to Provisioner class

--- src/Provisioner.php
+++ src/Provisioner.php
@@ -6,26 +6,131 @@
 namespace JPry\VVVBase;
 
 
+use Symfony\Component\Process\ProcessBuilder;
+use Symfony\Component\Process\Process;
+
 class Provisioner
 {
-    protected $site_name;
+    protected $builder;
+    protected $db;
     protected $config;
-    protected $options;
+    protected $site_name;
+    protected $site;
+    protected $vm_dir;
 
-    public function __construct($options)
+    /**
+     * Provisioner constructor.
+     *
+     * @param ProcessBuilder $builder
+     * @param \mysqli        $db
+     * @param array          $site_config
+     */
+    public function __construct(ProcessBuilder $builder, \mysqli $db, array $site_config)
     {
-        $this->options = $options;
-        $this->parseOptions();
+        $this->builder = $builder;
+        $this->db = $db;
+        $this->config = $site_config;
+        $this->setupSite();
     }
 
+    /**
+     * Provision the site.
+     */
     public function provision()
     {
+        $this->createDB();
+        $this->createLogs();
+        $this->downloadWordPress();
+        $this->createWpConfig();
+        $this->installWordPress();
+        $this->createNginxConfig();
+    }
+
+
+    protected function setupSite()
+    {
+        $this->validateOptions();
+        $this->validateSiteConfig();
+    }
+
+    /**
+     * Create the database.
+     */
+    protected function createDB()
+    {
+        echo "Checking database for site...\n";
+        $result = $this->db->query("SHOW DATABASE LIKE '{$this->site_name}'");
+        if (empty($result)) {
+            echo "Creating DB for {$this->site_name}\n";
+            $this->db->query("CREATE DATABASE `{$this->site_name}`;");
+            echo "Granting privileges on DB...\n";
+            $this->db->query("GRANT ALL PRIVILEGES ON `{$this->site_name}`.* TO wp@localhost IDENTIFIED BY 'wp'");
+            echo "DB setup complete.\n";
+        }
+    }
+
+    /**
+     * Create the log files and directory.
+     */
+    protected function createLogs()
+    {
 
     }
 
 
-    protected function parseOptions()
+    protected function createNginxConfig()
+    {
+
+    }
+
+
+    protected function createWpConfig()
+    {
+
+    }
+
+
+    protected function downloadWordPress()
+    {
+
+    }
+
+    /**
+     * Get a Process object to run a command.
+     *
+     * @param array $positional Positional arguments.
+     * @param array $flags      Key/value flags for the command.
+     *
+     * @return Process
+     */
+    protected function getCmd(array $positional, $flags = array())
+    {
+        $this->builder->setArguments($positional);
+        foreach ($flags as $flag => $value) {
+            // Build flag, including value if truthy
+            $cmd = "--{$flag}" . ($value ? "={$value}" : '');
+            $this->builder->add($cmd);
+        }
+
+        return $this->builder->getProcess();
+    }
+
+
+    protected function installWordPress()
+    {
+
+    }
+
+
+    protected function validateOptions()
+    {
+
+    }
+
+
+    protected function validateSiteConfig()
     {
 
     }
 }
+
