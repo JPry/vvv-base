8b58283d10b02888b2290ade4951d69a955e0d1c
Jeremy Pry <jeremy.pry@gmail.com>
2017-05-30 00:30:11 -0400

    Add filesystem tests

--- tests/ProvisionerTest.php
+++ tests/ProvisionerTest.php
@@ -7,6 +7,8 @@ namespace JPry\VVVBase;
 
 use Monolog\Handler\NullHandler;
 use Monolog\Logger;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\ProcessBuilder;
 
@@ -20,6 +22,9 @@ class ProvisionerTest extends TestCase
     /** @var \mysqli */
     protected $db_mock;
 
+    /** @var  vfsStreamDirectory */
+    protected $root;
+
 
     public static function setUpBeforeClass()
     {
@@ -28,6 +33,9 @@ class ProvisionerTest extends TestCase
 
     public function setUp()
     {
+        // Wrap filesystem stream for cleaner testing.
+        $this->root = vfsStream::setup();
+
         if (null === $this->db_mock) {
             $this->db_mock = $this->getMockBuilder('\mysqli')
                                   ->disableOriginalConstructor()
@@ -75,5 +83,31 @@ class ProvisionerTest extends TestCase
 
         $this->assertArrayNotHasKey('admin_user', $value);
         $this->assertEquals('admin', $value['admin_user']);
+    }
+
+
+    public function testCreateLogs()
+    {
+        $this->assertFalse($this->root->hasChild('log'));
+        $vm_dir = $this->getPublicProperty('vm_dir');
+        $vm_dir->setValue($this->provisioner, $this->root->url());
+
+        $createLogs = $this->getPublicMethod('createLogs');
+        $createLogs->invoke($this->provisioner);
+        $this->assertTrue($this->root->hasChild('log'));
+        $this->assertTrue($this->root->hasChild('log/error.log'));
+        $this->assertTrue($this->root->hasChild('log/access.log'));
+    }
+
+
+    public function testCreateBaseDir()
+    {
+        $this->assertFalse($this->root->hasChild('htdocs'));
+        $base_dir = $this->getPublicProperty('base_dir');
+        $base_dir->setValue($this->provisioner, $this->root->url() . '/htdocs');
+
+        $createBaseDir = $this->getPublicMethod('createBaseDir');
+        $createBaseDir->invoke($this->provisioner);
+        $this->assertTrue($this->root->hasChild('htdocs'));
     }
 }
