50da4e81dd14d1688c0987493e495ed698afd826
Jeremy Pry <jeremy.pry@gmail.com>
2017-10-20 15:18:28 -0400

    Create Provisioner Container

---
+++ src/ProvisionContainer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace JPry\VVVBase;
+
+/**
+ * Provisioner Container.
+ *
+ * @package JPry\VVVBase
+ */
+class ProvisionContainer implements ProvisionerInterface
+{
+    /**
+     * Array of provisioner objects.
+     *
+     * @var ProvisionerInterface[]
+     */
+    protected $provisioners = array();
+
+    /**
+     * Provision each of our objects.
+     *
+     * @author Jeremy Pry
+     */
+    public function provision()
+    {
+        foreach ($this->provisioners as $provisioner) {
+            $provisioner->provision();
+        }
+    }
+
+    /**
+     * Add a provisioner object.
+     *
+     * @author Jeremy Pry
+     *
+     * @param ProvisionerInterface $provisioner
+     */
+    public function addProvisioner(ProvisionerInterface $provisioner)
+    {
+        $this->provisioners[] = $provisioner;
+    }
+}
